1910756601634072da5f26e49df3ef114cefe359
SGGIRBS <ben_briggs@outlook.com>
2021-01-12 18:40:53 +0000

    Deployment Testing

--- localWhatIfDeploy.sh
+++ localWhatIfDeploy.sh
@@ -1,6 +1,6 @@
 az group create \
   --name ModernDW-rg \
   --location "West Europe"
-az deployment group create \
+az deployment group what-if \
   --resource-group ModernDW-rg \
   --template-file "azuredeploy.json"
